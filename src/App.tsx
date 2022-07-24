a1137c7c7f83fb6e1ddb3e1ef82dec5a529bca91
Albert Lin <albert@dinngo.co>
2022-07-24 23:57:53 +0800

    feat: add welcome-ui package

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ReactElement } from 'react';
 import styled from 'styled-components';
 import { ActivateDeactivate } from './components/ActivateDeactivate';
+import { ActivateDeactivate2 } from './components/ActivateDeactivate2';
 import { RunScript } from './components/RunScript';
 import { SectionDivider } from './components/SectionDivider';
 import { WalletStatus } from './components/WalletStatus';
-
+import { createTheme, WuiProvider } from '@welcome-ui/core'
+import { darkTheme } from '@welcome-ui/themes'
+const theme = createTheme(darkTheme)
 
 const StyledAppDiv = styled.div`
   display: grid;
@@ -13,12 +16,15 @@ const StyledAppDiv = styled.div`
 
 export function App(): ReactElement {
   return (
+    <WuiProvider theme={theme}>
     <StyledAppDiv>
+      <ActivateDeactivate2 />
       <ActivateDeactivate />
       <SectionDivider />
       <WalletStatus />
       <SectionDivider />
       <RunScript />
     </StyledAppDiv>
+    </WuiProvider>
   );
 }
