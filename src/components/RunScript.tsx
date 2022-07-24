8e5b401fce9c3bfb2b7865d991a1823275fc2622
Albert Lin <albert@dinngo.co>
2022-07-24 19:30:59 +0800

    feat: add run script architecture

---
+++ src/components/RunScript.tsx
@@ -0,0 +1,103 @@
+import { useWeb3React } from '@web3-react/core';
+import { Contract, ethers, Signer } from 'ethers';
+import {
+  ChangeEvent,
+  MouseEvent,
+  ReactElement,
+  useEffect,
+  useState
+} from 'react';
+import Select from 'react-select';
+
+import styled from 'styled-components';
+import { Provider } from '../utils/provider';
+import {SCRIPTS_MAPS} from '../scripts/default'
+
+
+
+
+const StyledGreetingDiv = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-columns: 300px 2.7fr 1fr;
+  grid-gap: 10px;
+  place-self: center;
+  align-items: center;
+`;
+
+const StyledButton = styled.button`
+  width: 150px;
+  height: 2rem;
+  border-radius: 1rem;
+  border-color: blue;
+  cursor: pointer;
+`;
+
+export function RunScript(): ReactElement {
+  const context = useWeb3React<Provider>();
+  const { library } = context;
+
+  const [signer, setSigner] = useState<Signer>();
+  const [scriptInput, setScriptInput] = useState<string>('');
+
+
+  useEffect((): void => {
+    if (!library) {
+      setSigner(undefined);
+      return;
+    }
+
+    setSigner(library.getSigner());
+  }, [library]);
+
+  function listOptions() {
+    var options = []
+    for(let key of Array.from( SCRIPTS_MAPS.keys()) ) {
+      var option = {
+        value: key, label: key
+      }
+      options.push(option)
+   }
+    return options;
+  }
+
+
+  async function runSubmit(event: MouseEvent<HTMLButtonElement>){
+    event.preventDefault();
+    if (!signer) {
+      window.alert("not connect wallet yet");
+      return;
+    }
+    await (await (SCRIPTS_MAPS.get(scriptInput)))?.default(signer);
+  }
+
+  function handleChange (option: any) {
+    console.log(option.value);
+    setScriptInput(option.value);
+  }
+
+  return (
+
+    <>
+
+
+      <StyledGreetingDiv>
+      <Select
+        defaultValue={{label: "Choose the script"}}
+        onChange={handleChange}
+        options={listOptions()}
+      />
+
+      <StyledButton
+          style={{
+            borderColor: 'unset'
+          }}
+          onClick={runSubmit}
+        >
+          Run Script
+      </StyledButton>
+      </StyledGreetingDiv>
+
+    </>
+  );
+}
---
+++ src/scripts/default.ts
@@ -0,0 +1,4 @@
+// Add Scripts Here
+export const SCRIPTS_MAPS = new Map([
+  ['01 - swap USDC to dai through Quickswap', import('./01-swap-usdc-to-dai-through-quickswap')],
+]);
